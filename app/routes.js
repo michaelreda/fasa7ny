9dc4de4656c34f45ebe89ebac07dde937b1431ca
sherrieKareem <sherrieKareem@gmail.com>
2017-03-30 13:07:09 +0200

    Login Method using passport

--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,12 @@ var express= require('express');
 var Router = express.Router();
 var mongoose = require('mongoose');
 var visitorCTRL= require('./controllers/visitorCTRL');
+var userCTRL= require('./controllers/userCTRL');
 
 // router configuration
 Router.post('/',visitorCTRL.shareActivityOnFacebook);
+
+Router.get('/user_Login',userCTRL.login );
 
 
 //exporting to be used in server.js
