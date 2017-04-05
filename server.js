f51ad44a215538835bc62a3a80c8c0e21818b09a
Michael Reda <michaelreda@gmail.com>
2017-04-06 01:01:10 +0200

    testing,validating and handling exceptions in adminCTRL
    testing,validating and handling exceptions in adminCTRL

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ var DB_URI =  "mongodb://admin:admin@ds147920.mlab.com:47920/fasa7ny";
 var passport = require('passport');
 var schedule = require('node-schedule');
 var globalCTRL = require('./app/controllers/globalCTRL');
-
+var expressValidator = require('express-validator');
 
 //configure app
 app.use(bodyParser.urlencoded({extended:true})); //this line must be on top of app config
 app.use(bodyParser.json());
+app.use(expressValidator());
 var job1 = schedule.scheduleJob('59 23 * * *', globalCTRL.banDecrement);
 var job2 = schedule.scheduleJob('59 23 * * 6', globalCTRL.sendNewsletter);
 var job3 = schedule.scheduleJob('59 23 * * *', globalCTRL.overdueBookings);
