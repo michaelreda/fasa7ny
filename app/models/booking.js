505f3b93834e5baa011e5a325ad746ebc9ad3f98
Michael Reda <michaelreda@gmail.com>
2017-04-06 22:16:13 +0200

    auto intending models

--- app/models/booking.js
+++ app/models/booking.js
@@ -1,41 +1,42 @@
 var mongoose = require('mongoose');
 
 var bookingSchema = mongoose.Schema({
-userId:{
-  type:mongoose.Schema.Types.ObjectId,ref:'user',
-  required:true
-},
-serviceProviderId:{
-  type:mongoose.Schema.Types.ObjectId,ref:'serviceProvider',
-  required:true
-},
-activityId:{
-  type:mongoose.Schema.Types.ObjectId,ref:'activity',
-  required:true
-},
-isHolding:{
-  type:Boolean,
-  required:true //??
-},
-price:{
-  type:Number,
-  required:true
-},
-time:{
-  type:Date,
-  required:true
-},
-isConfirmed:{
-  type:Boolean,
-  required:true,
-  default:false
-},
-isCancelled:{
-  type:Boolean,
-  required:true,
-  default:false
-}})
+  userId:{
+    type:mongoose.Schema.Types.ObjectId,ref:'user',
+    required:true
+  },
+  serviceProviderId:{
+    type:mongoose.Schema.Types.ObjectId,ref:'serviceProvider',
+    required:true
+  },
+  activityId:{
+    type:mongoose.Schema.Types.ObjectId,ref:'activity',
+    required:true
+  },
+  isHolding:{
+    type:Boolean,
+    required:true,
+    default: true
+  },
+  price:{
+    type:Number,
+    required:true
+  },
+  time:{
+    type:Date,
+    required:true
+  },
+  isConfirmed:{
+    type:Boolean,
+    required:true,
+    default:false
+  },
+  isCancelled:{
+    type:Boolean,
+    required:true,
+    default:false
+  }})
 
-var Booking = mongoose.model("booking", bookingSchema);
+  var Booking = mongoose.model("booking", bookingSchema);
 
-module.exports = Booking;
+  module.exports = Booking;
