93b5b2f7427b8be58a44ec6679eb2cec6bab2188
Michael Reda <michaelreda96@gmail.com>
2017-04-19 01:27:02 +0200

    added send gift function in front end

--- public/components/activityBox/activityBox.component.js
+++ public/components/activityBox/activityBox.component.js
@@ -1,12 +1,55 @@
 angular.module('myapp').
 component('activityBox',{
   templateUrl:'components/activityBox/activityBox.template.html',
-  controller: function ActivityBoxController($scope,$state){
+  controller: function ActivityBoxController($scope,$state, $uibModal){
       $scope.openActivity = function(activityID){
         $state.go("activity",{activityID:activityID})
       }
+
+      $scope.showSendGiftModal = function(){
+        $uibModal.open({
+          templateUrl: 'sendGiftModal.html',
+          controller: 'sendGiftModalController',
+        })
+        .result.then(
+          function () {
+          //  alert("OK");
+          },
+          function () {
+          //  alert("Cancel");
+          }
+        );
+      }
+
   },
    bindings: {
      data: '='
    }
 })
+
+.controller("sendGiftModalController", function ($scope, $uibModalInstance,activitySRV) {
+  $scope.selectedUsername = undefined;
+  activitySRV.getUsernames().success(function(data){
+      $scope.usernames =[];
+      for(var i=0;i<data.usernames.length;i++){
+        $scope.usernames[i] = data.usernames[i].userName;
+      }
+      console.log($scope.usernames);
+  })
+  $scope.ok = function () {
+    alert($scope.selectedUsername);
+    $uibModalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+})
+
+.filter('contains', function() { //checks if selected userName is in usernames array
+  return function (array, needle) {
+    if(needle != undefined)
+      return array.indexOf(needle) >= 0;
+    return -1;
+  };
+});
