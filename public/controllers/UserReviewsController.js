164a774bc3dfde76f7b7f251333518eb4b6260c8
Michael Reda <michaelreda96@gmail.com>
2017-04-20 01:19:02 +0200

    canel editing review now works

--- public/controllers/UserReviewsController.js
+++ public/controllers/UserReviewsController.js
@@ -10,5 +10,12 @@ myapp.controller('UserReviewsController', function($scope,userSRV) {
     document.getElementById("review_"+i).setAttribute("contenteditable", "true");
     document.getElementById("review_"+i).focus();
     $scope.reviewEditable[i]=true;
-  }
+  };
+
+  $scope.cancelEditing = function(i){
+    document.getElementById("review_"+i).setAttribute("contenteditable", "false");
+    var $review_block = $("#review_"+i).html($scope.reviews[i].review);
+    $scope.reviewEditable[i]=false;
+  };
+
 });
