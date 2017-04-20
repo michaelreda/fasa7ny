9a5968555293424e3461c11b2534ff8437260435
Unknown <karim.hanna@student.guc.edu.eg>
2017-04-20 21:42:46 +0200

    change privacy done and tested

--- public/controllers/userPageController.js
+++ public/controllers/userPageController.js
@@ -1,4 +1,12 @@
-myapp.controller('userPageController', function($scope,logInSRV, $state){
+myapp.controller('userPageController', function($scope,userSRV) {
+    var self=this;
+    self.privacyLevel=0;
+  
 
-
+  $scope.changePrivacy= function(privacyLevel){
+    self.privacyLevel=privacyLevel;
+    userSRV.changePrivacy(privacyLevel).success(function(){
+        console.log('changed privacy');
+    })
+  }
 });
