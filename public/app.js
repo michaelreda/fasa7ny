315dc9b2ba75d86edb50fb91fb2908d0f084f7b3
JoeRoumy <JoeRoumy2011@gmail.com>
2017-04-22 01:21:55 +0200

    app.js

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ var myapp= angular.module("myapp", ['ui.router','geolocation','yaru22.angular-ti
 myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
   $stateProvider.state({
     name:'home',
+    cache:false,
     url:'/',
     templateUrl:'views/home.view.html',
     controller:'HomeController'
@@ -10,22 +11,135 @@ myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$url
 
   $stateProvider.state({
     name:'activities',
+    cache:false,
     url:'/activities/:searchInput/:day/',
     templateUrl:'views/activities.view.html',
     controller:'ActivitiesController'
   });
 
-
-
   $stateProvider.state({
     name:'activity',
+    cache:false,
     url:'/activity/:activityID',
     templateUrl:'views/activity.view.html',
     controller:'ActivityController'
   });
 
   $stateProvider.state({
+    name:'userReviews',
+    url:'/my_reviews',
+    cache:false,
+    templateUrl:'views/userReviews.view.html',
+    controller:'UserReviewsController'
+  });
+
+  $stateProvider.state({
+    name:'userBookings',
+    cache:false,
+    url:'/my_bookings/:userID',
+    templateUrl:'views/userBookings.view.html',
+    controller:'UserBookingsController'
+  });
+  $stateProvider.state({
+    name:'booking',
+    cache:false,
+    url:'/booking/:activityID',
+    templateUrl:'views/booking.view.html',
+    controller:'BookingController'
+  });
+
+  $stateProvider.state({
+     name:'logIn',
+     cache:false,
+     url:'/login',
+     templateUrl:'views/loginPage.view.html',
+     controller:'logInController'
+   });
+
+   $stateProvider.state({
+     name:'contactPlatform',
+     cache:false,
+     url:'/contact_platform',
+     templateUrl:'views/contactPlatform.view.html',
+     controller:'contactPlatformController'
+   });
+
+   $stateProvider.state({
+     name:'userPage',
+     cache:false,
+     url:'/user',
+     templateUrl:'views/userPage.view.html',
+     controller:'userPageController'
+   });
+
+   $stateProvider.state({
+     name:'spPage',
+     cache:false,
+     url:'/serviceProvider',
+     templateUrl:'views/serviceProviderPage.view.html',
+     controller:'serviceProviderPageController'
+   });
+
+   $stateProvider.state({
+     name:'admin',
+     cache:false,
+     url:'/admin',
+     templateUrl:'views/adminPage.view.html',
+     controller:'adminPageController'
+   });
+
+   $stateProvider.state({
+     name:'systemLogs',
+     cache:false,
+     url:'/view_system_logs',
+     templateUrl:'views/systemLogs.view.html',
+     controller:'systemLogsController'
+      });
+
+   $stateProvider.state({
+     name:'signupLocal',
+     cache:false,
+     url:'/signup',
+     templateUrl:'views/signup.view.html',
+     controller:'signupController'
+  });
+
+
+  $stateProvider.state({
+    name:'newsletter',
+    cache:false,
+    url:'/newsletter',
+    templateUrl:'views/home.view.html',
+    controller:'NewsLetterController'
+  });
+
+   $stateProvider.state({
+     name: 'FAQ',
+     cache:false,
+     url: '/FAQ',
+     templateUrl:'views/FAQ.view.html',
+     controller: 'FAQController'
+   });
+
+   $stateProvider.state({
+     name: 'service providers',
+     cache:false,
+     url: '/serviceProviders',
+     templateUrl:'views/serviceProviders.view.html',
+     controller: 'SPsController'
+   });
+
+   $stateProvider.state({
+     name: 'service provider',
+     cache:false,
+     url: '/serviceProvider',
+     templateUrl:'views/serviceProvider.view.html',
+     controller: 'SPController'
+   });
+
+  $stateProvider.state({
     name:'aboutUs',
+     cache:false,
     url:'/aboutUs',
     templateUrl:'views/aboutUs.view.html'
   });
@@ -34,6 +148,7 @@ myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$url
 
   $stateProvider.state({
     name:'buttons',
+     cache:false,
     url:'/buttons',
     templateUrl:'views/buttons.view.html',
     controller:'buttonsController'
@@ -41,6 +156,7 @@ myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$url
 
   $stateProvider.state({
     name:'analysis',
+     cache:false,
     url:'/adminAnalysis',
     templateUrl:'views/analysis.view.html',
     controller:'analysisController'
@@ -48,6 +164,7 @@ myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$url
 
     $stateProvider.state({
     name:'filteredActivities',
+     cache:false,
     url:'/filteredActivities/:filter/:value/',
     templateUrl:'views/activities.view.html',
     controller:'ActivitiesController'
@@ -56,6 +173,7 @@ myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$url
       $stateProvider.state({
     name:'compare',
     url:'/comparison',
+     cache:false,
     templateUrl:'views/compare.view.html',
     controller:'compareController'
   });
@@ -63,105 +181,10 @@ myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$url
    $stateProvider.state({
     name:'adminComplainPanel',
     url:'/complains',
+     cache:false,
     templateUrl:'views/complains.view.html',
     controller:'complainsController'
   });
-  
-   $stateProvider.state({
-    name:'userReviews',
-    url:'/my_reviews',
-    templateUrl:'views/userReviews.view.html',
-    controller:'UserReviewsController'
-  });
-
-  $stateProvider.state({
-    name:'userBookings',
-    url:'/my_bookings/:userID',
-    templateUrl:'views/userBookings.view.html',
-    controller:'UserBookingsController'
-  });
-  
-  $stateProvider.state({
-    name:'booking',
-    url:'/booking/:activityID',
-    templateUrl:'views/booking.view.html',
-    controller:'BookingController'
-  });
-  
- $stateProvider.state({
-    name:'logIn',
-    url:'/login',
-    templateUrl:'views/loginPage.view.html',
-    controller:'logInController'
-  });
-
-  $stateProvider.state({
-    name:'contactPlatform',
-    url:'/contact_platform',
-    templateUrl:'views/contactPlatform.view.html',
-    controller:'contactPlatformController'
-  });
-
-  $stateProvider.state({
-    name:'userPage',
-    url:'/user',
-    templateUrl:'views/userPage.view.html',
-    controller:'userPageController'
-  });
-
-  $stateProvider.state({
-    name:'spPage',
-    url:'/serviceProvider',
-    templateUrl:'views/serviceProviderPage.view.html',
-    controller:'serviceProviderPageController'
-  });
-
-  $stateProvider.state({
-    name:'admin',
-    url:'/admin',
-    templateUrl:'views/adminPage.view.html',
-    controller:'adminPageController'
-  });
-
-  $stateProvider.state({
-    name:'systemLogs',
-    url:'/view_system_logs',
-    templateUrl:'views/systemLogs.view.html',
-    controller:'adminPageController'
-     });
-
-  $stateProvider.state({
-    name:'signupLocal',
-    url:'/signup',
-    templateUrl:'views/signup.view.html',
-    controller:'signupController'
- });
-
-
- $stateProvider.state({
-   name:'newsletter',
-   url:'/newsletter',
-   templateUrl:'views/home.view.html',
-   controller:'NewsLetterController'
- });
-
-
-  $stateProvider.state({
-    name: 'FAQ',
-    url: '/FAQ',
-    templateUrl:'views/FAQ.view.html',
-    controller: 'FAQController'
-  });
-
-  $stateProvider.state({
-    name: 'service providers',
-    url: '/serviceProviders',
-    templateUrl:'views/serviceProviders.view.html',
-    controller: 'SPsController'
-  });
-
-
-
 
 
   $urlRouterProvider.when('','/');
