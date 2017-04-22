b3f3a0d77234defe21584b98b5c27c168f085f9b
Michael Reda <michaelreda96@gmail.com>
2017-04-23 01:21:09 +0200

    offer price is now on activity box

--- public/controllers/ActivitiesController.js
+++ public/controllers/ActivitiesController.js
@@ -6,11 +6,19 @@ myapp.controller('ActivitiesController', function($scope,$stateParams,$state,act
       loaderSRV.start();
       activitiesSRV.search($stateParams.searchInput,$stateParams.day)
       .success(function(data){
-        for(var i=0;i<data.offers.length;i++){
-          for(var j=0;j<data.offers.activities){
-            
+        let offers = data.offers;
+        let activities= data.activities;
+        for(var i=0;i<offers.length;i++){
+          for(var j=0;j<offers[i].activities.length;j++){
+            for(var k=0;k<activities.length;k++){
+                if(activities[k].isOffer && activities[k]._id.toString() == offers[i].activities[j].toString()){
+                  var oldPrice=activities[k].prices[0].price;
+                  activities[k].newPrice=oldPrice - offers[i].discount*oldPrice;
+                }
+            }
           }
-        };
+        }
+        $scope.activities=activities;
         loaderSRV.end();
       })
   }
@@ -20,9 +28,19 @@ myapp.controller('ActivitiesController', function($scope,$stateParams,$state,act
       loaderSRV.start();
       activitiesSRV.filter($stateParams.filter,$stateParams.value)
       .success(function(data){
-        console.log (data);
-        $scope.activities=data.activities;
-        console.log($scope.activities);
+        let offers = data.offers;
+        let activities= data.activities;
+        for(var i=0;i<offers.length;i++){
+          for(var j=0;j<offers[i].activities.length;j++){
+            for(var k=0;k<activities.length;k++){
+                if(activities[k].isOffer && activities[k]._id.toString() == offers[i].activities[j].toString()){
+                  var oldPrice=activities[k].prices[0].price;
+                  activities[k].newPrice=oldPrice - offers[i].discount*oldPrice;
+                }
+            }
+          }
+        }
+        $scope.activities=activities;
         loaderSRV.end();
       })
   }
