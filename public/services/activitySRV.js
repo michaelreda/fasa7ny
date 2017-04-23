4ec8cc07c4a97ff39a7026cdd40d6f00634789ba
Michael Reda <michaelreda96@gmail.com>
2017-04-23 03:29:47 +0200

    rate and review activity works now

--- public/services/activitySRV.js
+++ public/services/activitySRV.js
@@ -8,6 +8,9 @@ myapp.factory('activitySRV', function($http) {
     },
     getActivityReviews:function(activityID){
       return $http.post('/get_activity_reviews',{activityID});
+    },
+    rateReviewActivity:function(activityId,review,inputRating,rating,ratingCount){
+      return $http.post('/ratereview_activity',{activityId:activityId,inputRating:inputRating,review:review,rating:rating,ratingCount:ratingCount});
     }
   };
 });
