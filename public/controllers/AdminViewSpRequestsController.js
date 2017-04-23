5c62b641900a257aeb998f998ae28208a3450bb7
Unknown <michael.zakhary@student.edu.eg>
2017-04-23 18:30:50 +0200

    admin view service provider requests controller

--- public/controllers/AdminViewSpRequestsController.js
+++ public/controllers/AdminViewSpRequestsController.js
@@ -1,4 +1,4 @@
-myapp.controller('UserReviewsController', function($scope,userSRV) {
+myapp.controller('UserReviewsController', function($scope,adminSRV) {
   userSRV.viewServiceProviderRequests().success(function(data){
     $scope.reviewEditable= new Array(data.length).fill(false);
     for(var i=0;i<data.length;i++){
