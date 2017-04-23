d7ea057fdf580cf3fbecfd7c7b60a8a06699ea94
Michael Morcos <Michael.zakhary@student.guc.edu.eg>
2017-04-23 19:55:57 +0200

    setting up accept and reject functions in controller

--- public/controllers/AdminViewSpRequestsController.js
+++ public/controllers/AdminViewSpRequestsController.js
@@ -4,36 +4,19 @@ myapp.controller('AdminViewSpRequestsController', function($scope,adminSRV) {
     $scope.requests= data;
   });
 
-  $scope.enableUpdateReview = function(i){
-    document.getElementById("review_"+i).setAttribute("contenteditable", "true");
-    document.getElementById("review_"+i).focus();
-    $scope.reviewEditable[i]=true;
-  };
-
-  $scope.cancelEditing = function(i){
-    document.getElementById("review_"+i).setAttribute("contenteditable", "false");
-    var $review_block = $("#review_"+i).html($scope.reviews[i].review);
-    $scope.reviewEditable[i]=false;
-    toastr.info('Review update cancelled');
-  };
-
-  $scope.update= function(i){
-    var review = document.getElementById("review_"+i).innerHTML;
-    var id= $scope.reviews[i]._id;
-    userSRV.updateReview(id,review).success(function(){
-      $scope.reviews[i].review=review;
-      document.getElementById("review_"+i).setAttribute("contenteditable", "false");
-      var $review_block = $("#review_"+i).html($scope.reviews[i].review);
-      $scope.reviewEditable[i]=false;
-      toastr.success('Review updated succesfully');
+  $scope.accept= function(i){
+    var id= $scope.requests[i]._id;
+    adminSRV.acceptServiceProviderRequests(id).success(function(){
+      toastr.success('service provider request accepted');
+      $state.go('viewSPrequests');
     })
   }
 
   $scope.refuse= function(i){
-    var id= $scope.reviews[i]._id;
-    userSRV.deleteReview(id).success(function(){
-      document.getElementById("row_"+i).remove();
-      toastr.success('Request refused');
+    var id= $scope.requests[i]._id;
+    adminSRV.rejectServiceProviderRequests(id).success(function(){
+      toastr.success('service provider request rejjected');
+      $state.go('viewSPrequests');
     })
   }
 
