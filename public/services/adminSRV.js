1ee082f1e180f5801c2705a1f278ba3f22ce3143
Michael Reda <michaelreda96@gmail.com>
2017-04-24 05:38:34 +0200

    admin respond to sp requests, service done

--- public/services/adminSRV.js
+++ public/services/adminSRV.js
@@ -2,6 +2,12 @@ myapp.factory('adminSRV', function($http) {
   return {
     viewServiceProviderRequests: function() {
       return $http.get('/view_sp_requests');
+    },
+    acceptServiceProviderRequests: function(id){
+      return $http.post('/accept_sp_requests',{serviceProviderId:id});
+    },
+    rejectServiceProviderRequests: function(id){
+      return $http.post('/reject_sp_requests',{serviceProviderId:id});
     }
   };
 });
