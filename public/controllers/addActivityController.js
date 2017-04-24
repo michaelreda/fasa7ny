ba9b7e19c42923e40fcd0b17cd693b0d05092e7c
Michael Reda <michaelreda96@gmail.com>
2017-04-24 20:39:41 +0200

    fixing price of add activity

--- public/controllers/addActivityController.js
+++ public/controllers/addActivityController.js
@@ -1,5 +1,22 @@
-myapp.controller('addActivityController', function($scope, $state, activitySRV){
+myapp.controller('addActivityController', function($scope, $state, activitySRV,$window){
+  $scope.prices=[];
+
   $scope.addActivity=function(){
+    if($window.localStorage['spProfile'] == undefined){
+      toastr.error("please login as sp");
+      return;
+    }
+    var price =[];
+    var numberOfClients=[];
+    for(var i=0;i<10;i++){
+      if(document.getElementById('price_'+i) !=undefined){
+        price[i]=document.getElementById('price_'+i).value;
+        numberOfClients[i]=document.getElementById('numberOfClients_'+i).value;
+      }else{
+        break;
+      }
+    }
+    alert(price);
     activitySRV.addActivity($scope.title, $scope.type, $scope.durationInMinutes,$scope.minClientNumber,$scope.maxClientNumber,$scope.minAge,$scope.maxAge,$scope.theme,$scope.prices).success(function(){
       toastr.success('Activity added succcessfully');
     })
