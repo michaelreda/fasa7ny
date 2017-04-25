4a16e2ccbf62e2dec2f0241643b129bd296a764c
JoeRoumy <JoeRoumy2011@gmail.com>
2017-04-26 00:48:20 +0200

    path

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.listen(process.env.PORT ||8080,function(){
 
 // API End Point - added by Stefan
 
-app.post('/webhook/', function (req, res) {
+app.post('/fb_bot', function (req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
