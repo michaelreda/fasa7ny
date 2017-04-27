57b10a96de450501e764a3516fc941cf75bdb903
Michael Reda <michaelreda96@gmail.com>
2017-04-27 18:04:39 +0200

    editing activeUser

--- app/models/BOT/activeUser.js
+++ app/models/BOT/activeUser.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 
 var activeUserSchema = mongoose.Schema({
-  facebookID: String,
-  firstName:String,
-  lastName:String,
-  location : {
-    lat:Number,
-    long:Number
-  },
-  gender: Boolean, //true -> male
+  // facebookID: String,
+  // firstName:String,
+  // lastName:String,
+  // location : {
+  //   lat:Number,
+  //   long:Number
+  // },
+  // gender: Boolean, //true -> male
+  botUser: {type:mongoose.Schema.Types.ObjectId,ref:'botUser'},
   currentScenario: {type:mongoose.Schema.Types.ObjectId,ref:'scenario'},
   NextScenarioMessage: {type:Number,default:-1}
 })
