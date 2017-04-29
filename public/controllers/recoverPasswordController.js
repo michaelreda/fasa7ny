fa48199d6a42c88a58477c46186cc626a5410174
sherrieKareem <sherrieKareem@gmail.com>
2017-04-29 21:15:04 +0200

    errors in recovering password

--- public/controllers/recoverPasswordController.js
+++ public/controllers/recoverPasswordController.js
@@ -2,19 +2,15 @@ myapp.controller('recoverPasswordController', function($scope, recoverPasswordSR
   $scope.recover=function(userName){
     if(userName==undefined){
       recoverPasswordSRV.recover(userName).success(function(){
-        console.log("No username");
         $state.go("recoverPassword");
         toastr.warning('No username is entered');
       })
     }
     else{
       recoverPasswordSRV.recover(userName).success(function(){
-        console.log("recovered");
         $state.go("home");
-        toastr.success('An e-mail was sent with your new password :) Please log in again');
+        toastr.success('An e-mail was sent with your new password :)Please log in again');
       })
     }
   }
-});
-
-//handle exceptions
+})
