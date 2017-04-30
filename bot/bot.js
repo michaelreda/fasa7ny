032b81945c4d4e3f570e2401cb69e2a508f1c944
Michael Reda <michaelreda96@gmail.com>
2017-04-30 03:44:29 +0200

    CHECKPOINT

--- bot/bot.js
+++ bot/bot.js
@@ -69,7 +69,7 @@ start_chatting = bot.on('message', (payload, chat) => {
             if(activeUser.currentScenario == undefined || activeUser.currentScenario == null){ //if no scenario at all then choose the welcoming scenario
               console.log("if no scenario at all then choose the welcoming scenario");
 
-              start_convo=()=>{ chat.conversation((convo) => {
+              chat.conversation((convo) => {
 
                 menu_payload = (menu_postback)=>{
                   if(menu_postback=='about_us'){
@@ -90,8 +90,7 @@ start_chatting = bot.on('message', (payload, chat) => {
                       convo.end();
                       convo.say("أهلا بك فى فسحنى تم تغيير اللغة").then(() => {
                     		// convo.ask(question1, answer1, callbacks1);
-                        console.log('1');
-                        start_convo();
+
                     	});
 
                     });
@@ -103,8 +102,7 @@ start_chatting = bot.on('message', (payload, chat) => {
                     BotUser.findOneAndUpdate(query, update, options,function(err,botUser){
                       convo.end();
                       convo.say("Now I can speak English with you ;)").then(() => {
-                        console.log('2');
-                        start_convo();
+                        
                     		// convo.ask(question1, answer1, callbacks1);
                     	});
                     });
@@ -350,8 +348,6 @@ start_chatting = bot.on('message', (payload, chat) => {
                convo.ask(question1, answer1, callbacks1); convo.ask(question1, answer1, callbacks1);
 
              });
-           }
-           start_convo();
 
 
 
