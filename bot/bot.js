e7ced0157ca866642e76c1e41518e1d26f567aff
Michael Reda <michaelreda96@gmail.com>
2017-04-30 03:16:16 +0200

    fixing translation bug

--- bot/bot.js
+++ bot/bot.js
@@ -69,7 +69,7 @@ start_chatting = bot.on('message', (payload, chat) => {
             if(activeUser.currentScenario == undefined || activeUser.currentScenario == null){ //if no scenario at all then choose the welcoming scenario
               console.log("if no scenario at all then choose the welcoming scenario");
 
-              chat.conversation((convo) => {
+              start_convo= chat.conversation((convo) => {
 
                 menu_payload = (menu_postback)=>{
                   if(menu_postback=='about_us'){
@@ -89,7 +89,8 @@ start_chatting = bot.on('message', (payload, chat) => {
                     BotUser.findOneAndUpdate(query, update, options,function(err,botUser){
                       convo.end();
                       convo.say("أهلا بك فى فسحنى تم تغيير اللغة").then(() => {
-                    		convo.ask(question1, answer1, callbacks1);
+                    		//convo.ask(question1, answer1, callbacks1);
+                        start_convo;
                     	});
 
                     });
@@ -101,7 +102,8 @@ start_chatting = bot.on('message', (payload, chat) => {
                     BotUser.findOneAndUpdate(query, update, options,function(err,botUser){
                       convo.end();
                       convo.say("Now I can speak English with you ;)").then(() => {
-                    		convo.ask(question1, answer1, callbacks1);
+                    		//convo.ask(question1, answer1, callbacks1);
+                        start_convo;
                     	});
                     });
                   }
